ee8b5657d084ebf7fa57d6d7d3c5720665b67aee
Lilpilot <xuzywozz@163.com>
2020-01-15 21:56:11 +0800

    feat:init project

---
+++ src/main/java/co/lilpilot/postcard/PostcardApplication.java
@@ -0,0 +1,13 @@
+package co.lilpilot.postcard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostcardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PostcardApplication.class, args);
+    }
+
+}
